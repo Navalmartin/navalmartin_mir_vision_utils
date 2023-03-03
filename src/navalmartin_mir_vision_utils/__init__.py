f03b8252006ad36ff1f5d4d1c2573b7355a13726
pockerman <a.giavaras@gmail.com>
2023-03-03 16:06:05 +0000

    Fix imports

--- src/navalmartin_mir_vision_utils/__init__.py
+++ src/navalmartin_mir_vision_utils/__init__.py
@@ -2,7 +2,12 @@
 Various utilities for working with images in the _mir_ project.
 """
 __version__ = "0.0.4"
-from .image_utils import load_img, ImageLoadersEnumType, is_valid_pil_image_from_bytes_string, show_pil_image
+from .image_utils import (load_img,
+                          ImageLoadersEnumType,
+                          is_valid_pil_image_from_bytes_string,
+                          is_valid_pil_image_file,
+                          show_pil_image,
+                          get_img_files, get_pil_image_size)
 from .image_transformers import pil2ndarray, pil_image_to_bytes_string
 from .image_enums import (ImageFileEnumType,
                           IMAGE_STR_TYPES,
--- src/navalmartin_mir_vision_utils/examples/image_utils_examples.py
+++ src/navalmartin_mir_vision_utils/examples/image_utils_examples.py
@@ -1,11 +1,12 @@
 from pathlib import Path
-from navalmartin_mir_vision_utils.image_utils import (is_valid_pil_image_file, get_pil_image_size,
-                                                      get_img_files)
-
+from navalmartin_mir_vision_utils import (is_valid_pil_image_file,
+                                          get_pil_image_size,
+                                          get_img_files)
 
 if __name__ == '__main__':
 
-    image = is_valid_pil_image_file(image=Path("/home/alex/qi3/mir-engine/datasets/cracks_v_3_id_8/train/cracked/img_9_9.jpg"))
+    image = is_valid_pil_image_file(
+        image=Path("/home/alex/qi3/mir-engine/datasets/cracks_v_3_id_8/train/cracked/img_9_9.jpg"))
     if image is not None:
         print("The provided image is OK")
         image_size = get_pil_image_size(image=image)
