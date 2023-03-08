4a2c667316b6ce9b09a659158a8c9dfdae5e9a11
pockerman <a.giavaras@gmail.com>
2023-03-08 18:02:04 +0000

    Add configuration file

---
+++ src/navalmartin_mir_vision_utils/mir_vision_config.py
@@ -0,0 +1,24 @@
+
+WITH_TORCH = False
+WITH_CV2 = False
+
+try:
+    import torch
+    import torchvision
+    from torchvision import transforms
+
+    WITH_TORCH = True
+except ModuleNotFoundError as e:
+    print(
+        f"WARNING: An exception was raised whilst importing torch, torchvision. Message {str(e)}")
+    print("WARNING: mir-vision-utils will not use torch and torchvision")
+    pass
+
+try:
+    import cv2
+    WITH_CV2 = True
+except ModuleNotFoundError as e:
+    print(
+        f"WARNING: An exception was raised whilst importing cv2 for OpenCV. Message {str(e)}")
+    print("WARNING: mir-vision-utils will not use OpenCV")
+    pass
