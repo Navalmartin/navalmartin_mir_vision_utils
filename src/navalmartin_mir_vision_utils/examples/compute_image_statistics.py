3db9fc2d0ee55c6081f1dcabadd03b3b7c1e0c11
pockerman <a.giavaras@gmail.com>
2023-03-08 18:01:39 +0000

    Add new example

---
+++ src/navalmartin_mir_vision_utils/examples/compute_image_statistics.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import numpy as np
+from navalmartin_mir_vision_utils import load_img, ImageLoadersEnumType
+from navalmartin_mir_vision_utils.statistics import compute_image_statistics, fit_gaussian_distribution_on_image
+
+if __name__ == '__main__':
+
+    image_path = Path("/home/alex/qi3/mir-engine/datasets/cracks_v_3_id_8/train/cracked/img_9_9.jpg")
+    image = load_img(path=image_path, loader=ImageLoadersEnumType.PIL)
+
+    print(f"Image size {image.size}")
+    print(f"Image bands {image.getbands()}")
+
+    image_stats = compute_image_statistics(image)
+    print(f"Image channel mean {image_stats.mean}")
+    print(f"Image channel var {image_stats.var}")
+    print(f"Image channel median {image_stats.median}")
+
+    channels_fit = fit_gaussian_distribution_on_image(image=image)
+    print(f"Gaussian distribution channel fit: {channels_fit}")
+
