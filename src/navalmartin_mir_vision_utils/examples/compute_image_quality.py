46124ba90441a49d65119c06b0f23934b839ac5a
pockerman <a.giavaras@gmail.com>
2023-03-28 22:11:40 +0100

    Update version strings

--- src/navalmartin_mir_vision_utils/examples/compute_image_quality.py
+++ src/navalmartin_mir_vision_utils/examples/compute_image_quality.py
@@ -4,7 +4,7 @@ from navalmartin_mir_vision_utils import load_img, ImageLoadersEnumType
 from navalmartin_mir_vision_utils.image_quality import brisque
 
 if __name__ == '__main__':
-    
+
     image_path = Path("/home/alex/qi3/mir_vision_utils/test_data/img_2_6.jpg")
     image = load_img(path=image_path, loader=ImageLoadersEnumType.PIL)
     brisque_score = brisque.score(image)
