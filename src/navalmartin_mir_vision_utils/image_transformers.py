95152d31db53d04373e092f02fa5d933c56c7c75
pockerman <a.giavaras@gmail.com>
2023-04-26 15:37:02 +0100

    Add pil to rgb abd grayscale utilities

--- src/navalmartin_mir_vision_utils/image_transformers.py
+++ src/navalmartin_mir_vision_utils/image_transformers.py
@@ -2,6 +2,7 @@ from PIL import Image
 import numpy as np
 import io
 
+from PIL import ImageOps
 
 def pil2ndarray(image: Image) -> np.ndarray:
     """Convert the Pillow image into a numpy array
@@ -35,3 +36,43 @@ def pil_image_to_bytes_string(image: Image) -> bytes:
     # Turn the BytesIO object back into a bytes object
     imgByteArr = img_byte_arr.getvalue()
     return imgByteArr
+
+
+def pil_to_rgb(image: Image) -> Image:
+    """Convert the PIL.Image to RGB. This function
+    can be used to convert a PNG image to JPG/JPEG
+    formats. Note that this function simply returns the
+    converted image. It does not save the newly formatted image
+
+    Parameters
+    ----------
+    image: The Image to convert
+
+    Returns
+    -------
+
+    An Instance of PIL.Image
+    """
+
+    # don't convert anything if the
+    # image is in the right mode
+    if image.mode == 'RGB':
+        return image
+
+    return image.convert("RGB")
+
+
+def pil_to_grayscale(img: Image) -> Image:
+    """Converts the given image to greyscale
+
+    Parameters
+    ----------
+    img: The image to convert to grayscale
+
+    Returns
+    -------
+
+    A grey-scaled image
+    """
+    # makes it greyscale
+    return ImageOps.grayscale(img)
